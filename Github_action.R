cd2bbf74598d8cfd7d6830ac68601b883e05dc45
Emil Thranholm <ethranholm@hotmail.com>
2022-04-24 14:14:08 +0200

    test1

--- Github_action.R
+++ Github_action.R
@@ -4,8 +4,7 @@
 install.packages("usethis")
 library(usethis)
 
-usethis::use_github_action("opdatering", 
-                           url = "https://github.com/Thranholm/R-test/tree/main/.github/workflows")
+use_github_action("opdatering")
 
 p_update(usethis, update = FALSE)
 
