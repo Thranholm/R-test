26ed466ea5f9445935a6e46ec7277449188f8da8
Emil Thranholm <ethranholm@hotmail.com>
2022-04-24 15:28:49 +0200

    testrender

--- Github_action.R
+++ Github_action.R
@@ -4,7 +4,16 @@
 install.packages("usethis")
 library(usethis)
 
-use_github_action("opdatering")
+
+
+
+
+
+use_github_action("opdatering",
+                  url = "https://github.com/Thranholm/R-test/blob/main/Corona-projekt.Rmd")
+
+
+
 
 p_update(usethis, update = FALSE)
 
